78e13cb607c545d683a2f9e075061d607768ee34
Franco Biasotti <francobiasottidev@gmail.com>
2022-10-24 12:09:13 -0300

    Update Tarea M6U2

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -14,4 +14,34 @@ async function deleteCalificacionById(id){
     return rows;
 }
 
-module.exports = {getCalificaciones, deleteCalificacionById}
+// Insertar novedades/comentarios...
+async function insertCalificacion(obj){
+    try{
+        var query = "insert into calificaciones set ?";
+        var rows = await pool.query(query, [obj]);
+        return rows;
+    } catch (error){
+        console.log(error);
+        throw error;
+    }
+}
+
+// Listar novedad/comentario por id...
+async function getCalificacionById(id){
+    var query = 'select * from calificaciones where id = ?';
+    var rows = await pool.query(query, [id]);
+    return rows[0];
+}
+
+// Modificar novedad/comentario...
+async function modificarCalificacionById(obj, id){
+    try{
+        var query = 'update calificaciones set ? where id = ?';
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
+    } catch (error){
+        throw error;
+    }
+}
+
+module.exports = {getCalificaciones, deleteCalificacionById, insertCalificacion, getCalificacionById, modificarCalificacionById}
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -21,4 +21,66 @@ router.get('/eliminar/:id', async (req, res, next)=>{
     res.redirect('/admin/novedades');
 }); //cierra get de eliminar
 
+// hbs para agregar las novedades
+router.get('/agregar', (req, res, next)=>{
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+    })
+}); // cierra get agregar
+
+// Para insertar novedad en la tabla
+router.post('/agregar', async (req, res, next)=>{
+    try{
+        if(req.body.calificacion != "" && req.body.titulo != "" && req.body.comentario != ""){
+            await novedadesModel.insertCalificacion(req.body);
+            res.redirect('/admin/novedades')
+        } else{
+            res.render('admin/agregar',{
+                layout: 'admin/layout',
+                error: true,
+                message: 'Todos los campos son requeridos'
+            })
+        }
+    } catch(error){
+        console.log(error)
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se cargo el comentario'
+        })
+    }
+})
+
+// Listar una sola novedad/comentario por id (para modificar)
+router.get('/modificar/:id', async (req, res, next)=>{
+    var id = req.params.id;
+    var comentario = await novedadesModel.getCalificacionById(id);
+
+    res.render('admin/modificar', {
+        layout: 'admin/layout',
+        comentario
+    })
+})
+
+// Modificar la novedad/comentario
+router.post('/modificar', async (req, res, next)=>{
+    try{
+        var obj = {
+            calificacion: req.body.calificacion,
+            titulo: req.body.titulo,
+            comentario: req.body.comentario
+        }
+
+        await novedadesModel.modificarCalificacionById(obj, req.body.id);
+        res.redirect('/admin/novedades');
+    } catch(error){
+        console.log(error)
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se modifico el comentario'
+        })
+    }
+})
+
 module.exports = router;
